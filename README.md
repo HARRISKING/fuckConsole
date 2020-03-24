73f4d8a368de9eca29d11ec26631274ca750a597
李连成 <liancheng.li@yunhutech.com>
2020-03-24 17:40:31 +0800

    update and finish

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# fuckConsole
+# fuckConsoleNode组件
+
+## 简介
+
+### 对于程序工程师来讲，console是最好的调试方法，直观又高效，然而每次提交到线上如果忘记删除代码中的console，又感觉像吃了苍蝇，所以这个工具意在每次执行线上打包时自动帮你检索并删除所有的`console.log`。
+
+## 使用方法
+### 1，将fuckConsole.js文件拷贝到需要处理的根目录；
+### 2，修改文件中`modify_here`处，改为你需要处理的文件路径，例如你的文件目录是：
+```
+-index.js
+-src
+    --a.js
+    --b.js
+    --c.js
+```
+如上一级目录中是index.js、src文件夹，你希望处理src文件夹中的a.js,b.js,c.js。则你需要将fuckConsole.js文件拷贝到于src同级目录，并参照fuckConsole.js文件中`modify_here`处修改路径。
+
+### 3，执行代码`node fuckConsole.js`
+
+## 结尾语
+### 代码初级，有更好的方式欢迎沟通！
---
+++ fuckConsole.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const fs = require("fs");
+
+// modify_here
+const filePath = path.join(__dirname, 'src')
+//调用文件遍历方法
+fileDisplay(filePath);
+
+/**
+ * 文件遍历
+ * @param filePath 需要遍历的文件路径
+ */
+function fileDisplay(filePath) {
+    //根据文件路径读取文件，返回文件列表
+    fs.readdir(filePath, function (err, files) {
+        if (err) {
+            console.warn(err)
+        } else {
+            //遍历读取到的文件列表
+            files.forEach(function (filename) {
+                if (filename !== "fuckConsole.js") {
+                    //获取当前文件的绝对路径
+                    var filedir = path.join(filePath, filename);
+                    //根据文件路径获取文件信息，返回一个fs.Stats对象
+                    fs.stat(filedir, function (eror, stats) {
+                        // 
+                        if (eror) {
+                            console.warn('获取文件stats失败');
+                        } else {
+                            var isFile = stats.isFile();//是文件
+                            var isDir = stats.isDirectory();//是文件夹
+                            if (isFile) {
+                                startFucking(filedir)
+                            } else if (isDir) {
+                                fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+                            }
+                        }
+                    })
+                }
+
+            });
+        }
+    });
+}
+
+/**
+ * 删除文件中所有的console
+ * @param {*} fileTarget 需要处理的文件路径 
+ */
+function startFucking(fileTarget) {
+    const context = fs.readFileSync(fileTarget, 'utf-8');
+    var newValue = context.replace(/console.log\(.*?\)/g, '');
+    fs.writeFileSync(fileTarget, newValue, 'utf-8')
+}
